4bf5b87230c1867c33a82317e1aa06a13a932f1c
Freezemage <freezemage0@gmail.com>
2023-06-06 00:55:37 +0300

    Added SynopsisOnly compiler.

--- src/Bot.php
+++ src/Bot.php
@@ -6,6 +6,7 @@ namespace Freezemage\LookupBot;
 use Discord\Builders\MessageBuilder;
 use Discord\Parts\Channel\Message;
 use Freezemage\LookupBot\Documentation\Compiler\Descriptive;
+use Freezemage\LookupBot\Documentation\Compiler\SynopsisOnly;
 
 
 class Bot
@@ -18,12 +19,15 @@ class Bot
     {
         $query = array_shift($arguments);
 
+        if (in_array('--short', $arguments)) {
+            $compiler = new SynopsisOnly();
+        } else {
+            $compiler = new Descriptive();
+        }
+
         $query = preg_replace('/[^A-Za-z0-9:\->_.]/', '', $query);
         $query = str_replace(['::', '->'], '.', $query);
 
-        var_dump($query);
-
-        $compiler = new Descriptive();
         $result = $this->service->findByDefinition($query, $compiler);
 
         $reply = MessageBuilder::new()->setContent($result);
---
+++ src/Documentation/Compiler/SynopsisOnly.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Freezemage\LookupBot\Documentation\Compiler;
+
+use Freezemage\LookupBot\Documentation\Compiler;
+use Freezemage\LookupBot\Documentation\Entry\ClassEntry;
+use Freezemage\LookupBot\Documentation\Entry\Extension;
+use Freezemage\LookupBot\Documentation\Entry\Method;
+
+
+class SynopsisOnly implements Compiler
+{
+    public function compileMethod(Method $method): string
+    {
+        $synopsis = array_map(
+                static fn (string $synopsis): string => "```php\n{$synopsis}\n```",
+                $method->synopsis
+        );
+
+        return implode("\n", $synopsis);
+    }
+
+    public function compileClass(ClassEntry $classEntry): string
+    {
+        // TODO: Implement compileClass() method.
+    }
+
+    public function compileExtension(Extension $extension): string
+    {
+        // TODO: Implement compileExtension() method.
+    }
+}
